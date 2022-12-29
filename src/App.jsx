2d86ce07522e31850ac31407bfa8a0617820b91a
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-12-29 20:27:29 +0700

    mulai buat fitur mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState,useEffect } from 'react'
+import ContainerMain from './component/container'
+import Container from './component/container'
 
 function App() {
       // useEffect(()=>{
       //   fetch('https://api.github.com/users').then(e=>e.json()).then(e=>{console.log(e)})
       // })
   return (
-    <div className="App relative w-full h-full bg-github-bg">
-     
+    <div className="App relative w-full h-[100vh] dark:bg-github-bg-dark">
+        <ContainerMain/>
     </div>
   )
 }
---
+++ src/component/container.jsx
@@ -0,0 +1,12 @@
+
+
+
+
+// component container 
+const ContainerMain = ()=>{
+    return (
+        <section className="Container relative max-w-lg border-2 border-red-600"></section>
+    )
+}
+
+export default ContainerMain;
