7e51ef27f2bc2410ac2388a53eb73c81d2a27ce8
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-12-29 19:29:01 +0700

    setup tailwind

--- src/README.md
+++ src/README.md
@@ -1,16 +1,6 @@
-colors
 
 
- --lm-bg: #f6f8ff;
-  --lm-bg-content: #fefefe;
-  --lm-text: #4b6a9b;
-  --lm-text-alt: #2b3442;
-  --lm-shadow: 0px 16px 30px -10px rgba(70, 96, 187, 0.2);
-  --lm-shadow-inactive: 0px 16px 30px -10px rgba(0, 0, 0, 0.2);
-  --lm-icon-bg: brightness(100%);
-  /* Btn */
-  --btn: #0079ff;
-  --btn-hover: #60abff;
+
+  github-icon-bg: brightness(100%);
 
 
-  font-family: 'Space Mono', monospace;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,24 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'space-mono': ['"Space Mono"']
+      },
+      colors:{
+        'github-bg': '#f6f8ff',
+  'github-bg-content': '#fefefe',
+  'github-text': '#4b6a9b',
+  'github-text-alt': '#2b3442',
+  'github-shadow': 'rgba(70, 96, 187, 0.2)',
+  'github-shadow-inactive': 'rgba(0, 0, 0, 0.2)',
+  'github-btn': '#0079ff',
+  'github-btn-hover': '#60abff',
+  'github-bg-dark':'#141C30',
+  'github-bg-content-dark':'#1F2A48',
+  'github-place-holder':'#49536C'
+      }
+    },
   },
   plugins: [],
 }
