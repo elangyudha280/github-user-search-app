03fc8063b2bb9caa51aa1e50ada509144ce26b4f
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-12-29 20:12:53 +0700

    mulai desain

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 function App() {
-
+      // useEffect(()=>{
+      //   fetch('https://api.github.com/users').then(e=>e.json()).then(e=>{console.log(e)})
+      // })
   return (
-    <div className="App">
+    <div className="App relative w-full h-full bg-github-bg">
      
     </div>
   )
