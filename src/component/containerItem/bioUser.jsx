356d34229ff258499aff7f66c9c74266c794d957
elangyudha280 <73148391+elangyudha280@users.noreply.github.com>
2024-11-13 05:00:10 -0800

    Update bioUser.jsx

--- src/component/containerItem/bioUser.jsx
+++ src/component/containerItem/bioUser.jsx
@@ -76,7 +76,7 @@ const BioUser = ({username,nickname,imgProfile,urlProfile,dateJoin,bio,repo,foll
                                 :
                                 (
                                     <a href={blog} className="text-website  max-w-[200px] min-[400px]flex-1 text-github-text-alt dark:text-white truncate hover:underline">
-                                        {}
+                                        {blog}
                                     </a>
                                 )
                             }
